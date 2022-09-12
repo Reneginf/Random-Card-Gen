d09647ff74d6abcbb5856d5bf951d188ed9d59a2
Reneginf <reneginf@gmail.com>
2022-09-12 15:12:03 +0000

    .

--- src/app.js
+++ src/app.js
@@ -2,10 +2,18 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let suit = ["♦", "♥", "♠", "♣"];
+  let number = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
+
+  let cardSuit = suit[Math.floor(Math.random() * suit.length)];
+  if (cardSuit == "♦" || cardSuit == "♥") {
+    document.querySelector(".container-fluid").style.color = "red";
+  }
+
+  let cardNumber = number[Math.floor(Math.random() * number.length)];
+
+  document.querySelector(".suitTop").innerHTML = cardSuit;
+  document.querySelector(".cardNumber").innerHTML = cardNumber;
+  document.querySelector(".suitBottom").innerHTML = cardSuit;
 };
